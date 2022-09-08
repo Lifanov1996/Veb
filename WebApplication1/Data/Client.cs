632d1bf96197665693748311d2fa3d8126bf36c4
Evgeny Lifanov <lifanov.j@yandex.ru>
2022-09-08 20:53:22 +0300

    Added class Client, to create list

---
+++ WebApplication1/Data/Client.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication1.Data
+{
+    public class Client
+    {
+        public int Id { get; }
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string Patronymic { get; }
+        public string NumberPhone { get; }
+        public string Address { get; }
+        public string Description { get; }
+
+        public Client (int id, string lastName, string firstName, string patronymic, string numberPhone, string address, string description)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            Patronymic = patronymic;
+            NumberPhone = numberPhone;
+            Address = address;
+            Description = description;
+        }
+
+        public Client (int id, string lastName, string firstName, string patronymic)
+        {
+            Id = id;
+            LastName = lastName;
+            FirstName = firstName;
+            Patronymic = patronymic;
+        }
+    }
+}
